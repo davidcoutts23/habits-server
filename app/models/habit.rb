75330c6c031d8495671f9581666ca76400413278
davidcoutts23 <david.coutts@envato.com>
2022-02-24 13:30:58 +1100

    passing test for habit model

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,3 +1,5 @@
 class Habit < ApplicationRecord
   belongs_to :HabitRating
+  has_many :ApplicationIntention
+  validates :name, presence: true
 end
--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Habit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+ it { should have_many(:ApplicationIntention) }
+ # Validation tests
+ it { should validate_presence_of(:name) }
 end

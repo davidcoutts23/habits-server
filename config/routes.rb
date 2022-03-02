0cc32534d7ac2335580b8099d99d01bb35a0a6b4
davidcoutts23 <david.coutts@envato.com>
2022-03-02 11:14:10 +1100

    add routes for habits

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
  namespace :api do
    namespace :v1 do
      resources :habit_rating, only: %i[index create destroy]
+     resources :habits, only: %i[index create destroy]
    end
  end
 end

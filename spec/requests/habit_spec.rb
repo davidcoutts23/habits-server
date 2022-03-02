c5400370a11c4fcb2b5210c8127b5418802b5696
davidcoutts23 <david.coutts@envato.com>
2022-03-02 22:13:55 +1100

    Create test for get habits

--- spec/requests/habit_spec.rb
+++ spec/requests/habit_spec.rb
@@ -1,18 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Habits", type: :request do
-  before do
-    HabitRating.new(rating: "Positive")
-    HabitRating.new(rating: "Negative")  
-    Habit.new(name: "Brush teeth", habit_rating_id: 1)
-    Habit.new(name: "Drink beer", habit_rating_id: 2)
-  end
+
+  let!(:habits) { create(:habit) }
 
   describe 'GET /habits' do
    before { get '/api/v1/habits' } 
     it 'returns habits' do
      expect(JSON.parse(response.body)).not_to be_empty
-     expect(JSON.parse(response.body).size).to eq(2)
+     expect(JSON.parse(response.body).size).to eq(1)
    end
    it 'returns status code 200' do
      expect(response).to have_http_status(200)

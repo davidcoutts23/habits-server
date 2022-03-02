60e993f87d16c3824608adbcf9614efed42d1fb8
davidcoutts23 <david.coutts@envato.com>
2022-03-02 22:27:00 +1100

    Add test for habit post

--- spec/requests/habit_spec.rb
+++ spec/requests/habit_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Habits", type: :request do
 
   let!(:habits) { create(:habit) }
+  # let!(:habit_id) { habits.first.id }
 
   describe 'GET /habits' do
    before { get '/api/v1/habits' } 
@@ -14,4 +15,27 @@ RSpec.describe "Habits", type: :request do
      expect(response).to have_http_status(200)
    end
   end
+
+  describe 'POST /habits/:id' do
+   let!(:habit_rating) { create(:habit_rating) }
+   let(:valid_attributes) do
+     { name: 'Brush my teeth',
+       habit_rating_id: habit_rating.id }
+   end
+   context 'when request attributes are valid' do
+     before { post '/api/v1/habits', params: valid_attributes }
+     it 'returns status code 201' do
+       expect(response).to have_http_status(201)
+     end
+   end
+   context 'when an invalid request' do
+     before { post '/api/v1/habits', params: {} }
+     it 'returns status code 422' do
+       expect(response).to have_http_status(422)
+     end
+     it 'returns a failure message' do
+       expect(response.body).to include("can't be blank")
+     end
+   end
+ end
 end

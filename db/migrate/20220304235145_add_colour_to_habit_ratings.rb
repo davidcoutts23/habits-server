688ca7dbd5270630e5696a34f8a88327849d4bbf
davidcoutts23 <david.coutts@envato.com>
2022-03-05 10:53:41 +1100

    Add colour to habit_ratings

---
+++ db/migrate/20220304235145_add_colour_to_habit_ratings.rb
@@ -0,0 +1,5 @@
+class AddColourToHabitRatings < ActiveRecord::Migration[7.0]
+  def change
+    add_column :habit_ratings, :colour, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,34 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_224_015_924) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_04_235145) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'application_intentions', force: :cascade do |t|
-    t.string 'behaviour'
-    t.string 'time'
-    t.string 'location'
-    t.bigint 'habit_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['habit_id'], name: 'index_application_intentions_on_habit_id'
+  create_table "application_intentions", force: :cascade do |t|
+    t.string "behaviour"
+    t.string "time"
+    t.string "location"
+    t.bigint "habit_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["habit_id"], name: "index_application_intentions_on_habit_id"
   end
 
-  create_table 'habit_ratings', force: :cascade do |t|
-    t.string 'rating'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "habit_ratings", force: :cascade do |t|
+    t.string "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "colour"
   end
 
-  create_table 'habits', force: :cascade do |t|
-    t.string 'name'
-    t.bigint 'habit_rating_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['habit_rating_id'], name: 'index_habits_on_habit_rating_id'
+  create_table "habits", force: :cascade do |t|
+    t.string "name"
+    t.bigint "habit_rating_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["habit_rating_id"], name: "index_habits_on_habit_rating_id"
   end
 
-  add_foreign_key 'application_intentions', 'habits'
-  add_foreign_key 'habits', 'habit_ratings'
+  add_foreign_key "application_intentions", "habits"
+  add_foreign_key "habits", "habit_ratings"
 end

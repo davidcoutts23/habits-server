10f0515012445a155257a2dfa8cfdb24294a1beb
davidcoutts23 <david.coutts@envato.com>
2022-03-04 12:02:43 +1100

    Test for application intention post endpoint

--- spec/requests/application_intention_spec.rb
+++ spec/requests/application_intention_spec.rb
@@ -15,26 +15,28 @@ RSpec.describe "ApplicationIntentions", type: :request do
    end
   end
 
-#   describe 'POST /habits/:id' do
-#    let!(:habit_rating) { create(:habit_rating) }
-#    let(:valid_attributes) do
-#      { name: 'Brush my teeth',
-#        habit_rating_id: habit_rating.id }
-#    end
-#    context 'when request attributes are valid' do
-#      before { post '/api/v1/habits', params: valid_attributes }
-#      it 'returns status code 201' do
-#        expect(response).to have_http_status(201)
-#      end
-#    end
-#    context 'when an invalid request' do
-#      before { post '/api/v1/habits', params: {} }
-#      it 'returns status code 422' do
-#        expect(response).to have_http_status(422)
-#      end
-#      it 'returns a failure message' do
-#        expect(response.body).to include("can't be blank")
-#      end
-#    end
-#  end
+  describe 'POST /application_intention/:id' do
+   let!(:habit) { create(:habit) }
+   let(:valid_attributes) do
+     { behaviour: 'Meditate',
+       time: 'Every morning after I wake up',
+       location: 'Lounge rooom',
+       habit_id: habit.id }
+   end
+   context 'when request attributes are valid' do
+     before { post '/api/v1/application_intentions', params: valid_attributes }
+     it 'returns status code 201' do
+       expect(response).to have_http_status(201)
+     end
+   end
+   context 'when an invalid request' do
+     before { post '/api/v1/application_intentions', params: {} }
+     it 'returns status code 422' do
+       expect(response).to have_http_status(422)
+     end
+     it 'returns a failure message' do
+       expect(response.body).to include("can't be blank")
+     end
+   end
+ end
 end

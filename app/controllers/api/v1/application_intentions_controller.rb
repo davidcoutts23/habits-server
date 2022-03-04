86194bbd011c6c612a0093c646070016f7e3723c
davidcoutts23 <david.coutts@envato.com>
2022-03-04 12:13:01 +1100

    Implement create application intention endpoint

--- app/controllers/api/v1/application_intentions_controller.rb
+++ app/controllers/api/v1/application_intentions_controller.rb
@@ -2,28 +2,28 @@ module Api
    module V1
      class ApplicationIntentionsController < ApplicationController
 
-      #Get /habits
+      #Get /application_intentions
       def index
         application_intentions_json = ApplicationIntentionBlueprint.render_as_json ApplicationIntention.all
         render json: application_intentions_json
       end
 
-#       # Post /habit
-#       def create
-#         @habit = Habit.new(habit_params)
-#         if @habit.save
-#           habits_json = HabitBlueprint.render_as_json Habit.find(@habit.id)
-#           render json: habits_json, status: :created
-#         else
-#           habits_json = @habit.errors
-#           render json: habits_json, status: :unprocessable_entity
-#         end
-#      end
-#      private
+      # Post /application_intentions
+      def create
+        @application_intention = ApplicationIntention.new(application_intention_params)
+        if @application_intention.save
+          application_intention_json = ApplicationIntentionBlueprint.render_as_json ApplicationIntention.find(@application_intention.id)
+          render json: application_intention_json, status: :created
+        else
+          application_intention_json = @application_intention.errors
+          render json: application_intention_json, status: :unprocessable_entity
+        end
+     end
+     private
 
-#       def habit_params
-#         params.permit(:name, :habit_rating_id)
-#       end
+      def application_intention_params
+        params.permit(:behaviour, :time, :location, :habit_id)
+      end
    end
 end
 end

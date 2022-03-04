b46252c996aa987036a88d4b8c8d34a5476ea86d
davidcoutts23 <david.coutts@envato.com>
2022-03-04 11:52:47 +1100

    Add get application intention test and factory

---
+++ spec/factories/application_intention.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+ factory :application_intention, class: ApplicationIntention do
+  behaviour { Faker::Lorem.words(number: 1) }
+  time { Faker::Lorem.words(number: 1) }
+  location { Faker::Address.city }
+  habit { create(:habit) }
+ end
+end
---
+++ spec/requests/application_intention_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "ApplicationIntentions", type: :request do
+
+  let!(:application_intention) { create(:application_intention) }
+
+  describe 'GET /application_intentions' do
+   before { get '/api/v1/application_intentions' } 
+    it 'returns application intentions' do
+     expect(JSON.parse(response.body)).not_to be_empty
+     expect(JSON.parse(response.body).size).to eq(1)
+   end
+   it 'returns status code 200' do
+     expect(response).to have_http_status(200)
+   end
+  end
+
+#   describe 'POST /habits/:id' do
+#    let!(:habit_rating) { create(:habit_rating) }
+#    let(:valid_attributes) do
+#      { name: 'Brush my teeth',
+#        habit_rating_id: habit_rating.id }
+#    end
+#    context 'when request attributes are valid' do
+#      before { post '/api/v1/habits', params: valid_attributes }
+#      it 'returns status code 201' do
+#        expect(response).to have_http_status(201)
+#      end
+#    end
+#    context 'when an invalid request' do
+#      before { post '/api/v1/habits', params: {} }
+#      it 'returns status code 422' do
+#        expect(response).to have_http_status(422)
+#      end
+#      it 'returns a failure message' do
+#        expect(response.body).to include("can't be blank")
+#      end
+#    end
+#  end
+end

9024855ebba179122ab7ff6fe3c8f343a218c896
davidcoutts23 <david.coutts@envato.com>
2022-03-05 10:55:33 +1100

    Add habit ratings to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+HabitRating.create(rating: "Positive", colour: "Success")
+HabitRating.create(rating: "Neutral", colour: "Warning")
+HabitRating.create(rating: "Negative", colour: "Danger")

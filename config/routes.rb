e949e78d9cce84ff060a52435cdf0dff6efbedbc
davidcoutts23 <david.coutts@envato.com>
2022-03-04 11:53:45 +1100

    Add application intention route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    namespace :v1 do
      resources :habit_rating, only: %i[index create destroy]
      resources :habits, only: %i[index create destroy]
+     resources :application_intentions, only: %i[index create destroy]
    end
  end
 end

e670d0032ac4c84b85724e93e5515b24d36b73a7
davidcoutts23 <david.coutts@envato.com>
2022-03-09 19:21:46 +1100

    Add user model and model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :habits
+    validates :email, presence: true, uniqueness: true, length: { minimum: 3 }
+    validates :password, presence: true, length: { minimum: 6 }
 end

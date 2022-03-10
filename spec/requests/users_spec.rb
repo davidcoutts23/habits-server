7dfc5d7b96b7a43ba1a54e45939f539d55a6e54c
davidcoutts23 <david.coutts@envato.com>
2022-03-10 17:22:08 +1100

    Fix invalid user attributes test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,19 +18,19 @@ RSpec.describe "Users", type: :request do
       it 'returns status code 201' do
         post '/api/v1/register', params: { user: valid_user_attributes } 
         expect(response).to have_http_status(201)
-        # expect(json).to eq({
-        #                   'id' => User.last.id,
-        #                   'email' => valid_user_attributes.email,
-        #                   'token' => AuthenticationTokenService.call(User.last.id)
-        #                 })
+        expect(json).to eq({
+                          'id' => User.last.id,
+                          'email' => 'test@gmail.com',
+                          # 'token' => AuthenticationTokenService.call(User.last.id)
+                        })
       end
     end
-    # context 'Register with invalid user attributes (Password length)' do
-    #   before { post '/api/v1/register', params: { user: { invalid_user_attributes_password_length } }
-    #   it 'returns status code 201' do
-    #     expect(response).to have_http_status(422)
-    #   end
-    # end
+    context 'Register with invalid user attributes (Password length)' do
+      it 'returns status code 201' do
+        post '/api/v1/register', params: { user: invalid_user_attributes_password_length } 
+        expect(response).to have_http_status(422)
+      end
+    end
   end
   
 end

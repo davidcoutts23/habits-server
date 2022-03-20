09b1f629430fe457da5809c5554187131aef4004
davidcoutts23 <david.coutts@envato.com>
2022-03-20 19:34:25 +1100

    Add new origin to cors.rb for heroku hosted client

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,4 +15,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              headers: :any,
              methods: %i[get post put patch delete options head]
   end
+
+  allow do
+    origins 'https://shrouded-wildwood-58915.herokuapp.com/'
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
+  end
 end

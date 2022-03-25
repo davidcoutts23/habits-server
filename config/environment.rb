c030044ae5aa0522250569a24f114f885c5592f0
davidcoutts23 <david.coutts@envato.com>
2022-03-25 15:10:16 +1100

    Configure and add log lines

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configure logger
+Rails.logger = Logger.new(STDOUT)
+Rails.logger.level = Logger::DEBUG

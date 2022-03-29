898daad8e173a61a25f7c7e756c396a95e48fff3
davidcoutts23 <david.coutts@envato.com>
2022-03-29 18:43:43 +1100

    Remove stdout logging config

--- config/environment.rb
+++ config/environment.rb
@@ -8,4 +8,8 @@ Rails.application.initialize!
 
 # Configure logger
 Rails.logger = Logger.new(STDOUT)
-Rails.logger.level = Logger::DEBUG
+# Rails.logger.level = Logger::DEBUG
+
+# logger = ActiveSupport::Logger.new($stdout)
+# logger.formatter = config.log_formatter
+# config.logger = logger

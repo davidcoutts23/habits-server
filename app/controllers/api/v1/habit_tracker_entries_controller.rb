e0cdc39887ae343d53cebba0481435006951dd45
davidcoutts23 <david.coutts@envato.com>
2022-04-28 16:56:02 +1000

    Add ability to request all habit tracker entries for a specific habit

--- app/controllers/api/v1/habit_tracker_entries_controller.rb
+++ app/controllers/api/v1/habit_tracker_entries_controller.rb
@@ -4,19 +4,28 @@ module Api
         before_action :authenticate_request!, :get_habit_tracker_entry, only: %i[update]
 
         def index
-            if request.query_parameters.any?
-              habit_tracker_entries = []
-                request.query_parameters.each do |scope, value|
-                    Habit.where(user_id: current_user!.id).each do |habit|
-                      habit.habit_tracker_entries.where(completion_status: value).each do |habit_tracker_entry|
-                        habit_tracker_entries.push(habit_tracker_entry)
-                      end
-                    end
-                end
-            end
+          habit_tracker_entries = []
 
-            habit_tracker_entries_json = HabitTrackerEntryBlueprint.render_as_json habit_tracker_entries
-            render json: habit_tracker_entries_json
+          if request.query_parameters.member?("habit_id")
+            habits = Habit.where(user_id: current_user!.id, id: request.query_parameters["habit_id"])
+          else
+            habits = Habit.where(user_id: current_user!.id)
+          end
+          
+          habits.each do |habit|
+            if request.query_parameters.member?("completion_status")
+              habit.habit_tracker_entries.where(completion_status: request.query_parameters["completion_status"]).each do |habit_tracker_entry|
+                habit_tracker_entries.push(habit_tracker_entry)
+              end
+            else
+              habit.habit_tracker_entries.each do |habit_tracker_entry|
+                habit_tracker_entries.push(habit_tracker_entry)
+              end
+            end
+          end
+          
+          habit_tracker_entries_json = HabitTrackerEntryBlueprint.render_as_json habit_tracker_entries
+          render json: habit_tracker_entries_json
         end
 
         def update
--- spec/requests/habit_tracker_entry_spec.rb
+++ spec/requests/habit_tracker_entry_spec.rb
@@ -5,15 +5,28 @@ RSpec.describe "HabitTrackerEntries", type: :request do
   let!(:habit) { create(:habit) }
   let!(:unprocessed_habit_tracker_entry) { create(:habit_tracker_entry, completion_status: "unprocessed")}
   let!(:processed_habit_tracker_entry) { create(:habit_tracker_entry, completion_status: "succeeded")}
+  let!(:processed_habit_tracker_entry) { create(:habit_tracker_entry, completion_status: "failed")}
 
-  describe 'GET /unprocessed_habit_tracker_entries' do
-    before { get '/api/v1/habit_tracker_entries?completion_status=unprocessed', headers: { 'Authorization' => AuthenticationTokenService.call(user.id)} }
-    it 'returns habit tracker entries with an unprocessed completion status' do
-      expect(JSON.parse(response.body)).not_to be_empty
-      expect(JSON.parse(response.body).size).to eq(1)
+  describe 'GET /habit_tracker_entries' do
+    context 'Get unprocessed habit tracker entries only' do
+      before { get '/api/v1/habit_tracker_entries?completion_status=unprocessed', headers: { 'Authorization' => AuthenticationTokenService.call(user.id)} }
+      it 'returns habit tracker entries with an unprocessed completion status' do
+        expect(JSON.parse(response.body)).not_to be_empty
+        expect(JSON.parse(response.body).size).to eq(1)
+      end
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
     end
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
+    context 'Get all habit tracker entries' do
+      before { get '/api/v1/habit_tracker_entries', headers: { 'Authorization' => AuthenticationTokenService.call(user.id)} }
+      it 'returns all habit tracker entry records' do
+        expect(JSON.parse(response.body)).not_to be_empty
+        expect(JSON.parse(response.body).size).to eq(3)
+      end
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
@@ -28,10 +41,9 @@ RSpec.describe "HabitTrackerEntries", type: :request do
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
-    # it 'updates habit' do
-    #   updated_item = Habit.find(habit.id)
-    #   expect(updated_item.name).to match(/Meditate daily/)
-    #   expect(updated_item.habit_rating_id).to eq(habit_rating.id)
-    # end
+    it 'updates habit tracker entry' do
+      updated_item = HabitTrackerEntry.find(unprocessed_habit_tracker_entry.id)
+      expect(updated_item.completion_status).to match(/succeeded/)
+    end
   end
 end
